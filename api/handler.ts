925cf0cb434fefa2fd89dd596b6a0fa6bf1daab5
Agustin Castiarena <castiarena@gmail.com>
2022-10-25 00:26:21 +0200

    adjust payment

--- api/handler.ts
+++ api/handler.ts
@@ -9,23 +9,27 @@ const app = express()
 
 const { AUTH_URL, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } = process.env
 
-const CLEINT_ID_AND_SECRET = `${CLIENT_ID}:${CLIENT_SECRET}`
+const CLIENT_ID_AND_SECRET = `${CLIENT_ID}:${CLIENT_SECRET}`
 
 const authApi = create({
   baseURL: AUTH_URL,
   headers: {
-    Authorization: 'Basic ' + Buffer.from(CLEINT_ID_AND_SECRET).toString('base64'),
+    Authorization: 'Basic ' + Buffer.from(CLIENT_ID_AND_SECRET).toString('base64'),
     'Content-Type': 'application/x-www-form-urlencoded',
   },
 })
 
 app.get('/auth', (req, res) => {
-  authApi
-    .post('/api/token', `grant_type=client_credentials&redirect_uri=${REDIRECT_URI}`)
-    .then(({ data }) => {
-      res.json(data)
-    })
-    .catch((e) => console.log(e))
+  const redirect_uri = `${req.protocol}://${req.get('host')}/login`
+
+  res.redirect(
+      `${AUTH_URL}/authorize?client_id=${CLIENT_ID}&response_type=code&scope=streaming&&redirect_uri=${redirect_uri}`
+  )
 })
+
+app.get('/login', (req, res) => {
+  res.json(req.query)
+})
+
 
 export const handler = app
